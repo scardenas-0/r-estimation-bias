a0e0f83ec174476bc9ddbe711485884df9538932
Santiago Cardenas <118599378+scardenas-0@users.noreply.github.com>
2025-08-28 17:03:14 -0700

    Resizing OR and vax plots

--- script_disp082425.m
+++ script_disp082425.m
@@ -4,51 +4,13 @@ path(path,'..')
 % Figure for showing what combinations of parameters would be compatible
 % with observations of measles
 
-d1 = load('Data/061925_msls_02_model');
-d2 = load('Data/061925_msls_02_perf');
+d1 = load('Data/082225_msls_r.mat');
+% d2 = load('Data/061925_msls_02_perf');
 R_inf = squeeze(d1.rs_inf_res)';
 
 figure(1),clf
 
 %Third colm of d2.res_arr: [C_pp C_ps C_sp, C_ss perf_p perf_s accur r_inf r_bias]
-
-
-% Prob primary cases are labeled as primary
-subplot (3,2,1)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,1)','ShowText','On','LineWidth',2)
-set(gca,'FontSize',12)
-title({'C_{p\rightarrow p}'})
-ylabel({'Observation_ ','probability (P_{obs})'})
-%xlabel({'              Primary cases per cluster (R_p)'})
-
-
-% Prob secondary cases are labeled as secondary
-subplot (3,2,2)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,4)','ShowText','On','LineWidth',2)
-set(gca,'FontSize',12)
-title({'C_{s\rightarrow s}'})
-%ylabel({'Observation_ ','probability (P_{obs})'})
-%xlabel({'              Primary cases per cluster (R_p)'})
-
-clf
-% Prob observed primary is a primary
-%subplot (3,2,3)
-subplot (2,2,1)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,5)','ShowText','On','LineWidth',2)
-set(gca,'FontSize',12)
-title({'P_{p\rightarrow p}'})
-%ylabel({'Observation_ ','probability (P_{obs})'})
-%xlabel({'              Primary cases per cluster (R_p)'})
-
-% Prob observed secondary is a secondary
-%subplot (3,2,4)
-subplot (2,2,2)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,6)','ShowText','On','LineWidth',2)
-set(gca,'FontSize',12)
-title({'P_{s\rightarrow s}'})
-%ylabel({'Observation_ ','probability (P_{obs})'})
-%xlabel({'              Primary cases per cluster (R_p)'})
-
 
 % From McQuillan et al., The Journal of Infectious Diseases, Volume 196, Issue 10, 15 November 2007, Pages 1459?1464
 % During 1999-2004, the rate of measles seropositivity in the population overall was 95.9% (95% confidence interval [CI], 95.1%?96.5%)
@@ -62,21 +24,8 @@ title({'P_{s\rightarrow s}'})
 % 1 = (.05+x)*R0 = (1 + x/.041)*R_inf
 % Thus x = .05(1/R_inf -1)*100
 
-% R effective
-%subplot (3,2,5)
-subplot (2,2,3)
-contour(d1.rp_arr,d1.p_obs_arr,R_inf,[0.75, 0.76, 0.77, 0.78, 0.79, 0.8],'ShowText','On','LineWidth',2)
-set(gca,'FontSize',12)
-title({'Effective reproduction number'})
-%ylabel({'Observation_ ','probability (P_{obs})'})
-%xlabel({'Primary cases per cluster (R_p)'})
-text(.96,.90,0,'Observation probability (P_{obs})','FontSize', 16,'Rotation', 90)
-text(1.25,0.15,0,'Primary cases per cluster (R_p)','FontSize', 16)
-
-%subplot (3,2,6)
-subplot (2,2,4)
 % contour(d1.rp_arr,d1.p_obs_arr,4.1*(1./R_inf -1),[1.1, 1.2, 1.4, 1.6, 1.8, 2],'ShowText','On','LineWidth',2)
-contour(d1.rp_arr,d1.p_obs_arr,5*(1./R_inf -1),[1, 1.1, 1.2, 1.3, 1.4, 1.45, 1.5],'ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,5.562864779*(1./R_inf -1),'ShowText','On','LineWidth',2)
 set(gca,'FontSize',12)
 title({'Absolute decrease in vaccination percentage','leading to critical transmission threshold'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
@@ -85,18 +34,9 @@ title({'Absolute decrease in vaccination percentage','leading to critical transm
 %set(tt, 'FontSize', 10, 'FontWeight','bold')
 %ylim([.5 1])
 
-% subplot (2,3,4)
-% contour(d1.rp_arr,d1.p_obs_arr,squeeze(compat_arr)','ShowText','On','LineWidth',2)
-% set(gca,'FontSize',12)
-% title({'Bias of R_s estimate for measles'})
-% ylabel({'Observation_ ','probability (P_{obs})'})
-% xlabel({'              Primary cases per cluster (R_p)'})
-% %tt = text(2.01, .9, {'Heterogenous','R_s = 0.8'})
-% %set(tt, 'FontSize', 10, 'FontWeight','bold')
-% ylim([.7 1])
-
 % savepdf('Figures/040519_msls_app')
-% exportgraphics(gcf, 'Figures/061925_msls_02_app.pdf')
+exportgraphics(gcf, 'Figures/082225_msls_vax_app.pdf')
+
 
 %%
 % Figure look at R and OR for measles in 2020s
@@ -109,49 +49,50 @@ R_inf = squeeze(d1.rs_inf_res)';
 
 % Prob observed primary is a primary
 subplot (3,2,1)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,5)','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,5)',[0.7, 0.8, 0.9, 1],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Probability that a case_ ','classified as a primary case_ ', 'is a primary case (P_{p\rightarrow p})'})
+title({'Probability that a case classified_ ','as primary is primary (P_{p\rightarrow p})'})
 %title({'P_{p\rightarrow p}'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
 
 % Prob observed secondary is a secondary
-subplot (3,2,2)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,6)','ShowText','On','LineWidth',2)
+subplot (3,2,3)
+contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,6)',[0.85, 0.875, 0.9, 0.925, 0.95, 0.975, 1],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Probability that a case_ ','classified as a secondary case_ ', 'is a secondary case (P_{s\rightarrow s})'})
+title({'Probability that a case classified_ ','as secondary is secondary (P_{s\rightarrow s})'})
 %title({'P_{s\rightarrow s}'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
+ylabel({'Observation probability (P_{obs})'},'FontSize',11)
 
-% Inferred proportion primary among positive
+% Inferred proportion positive among primary
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
-subplot (3,2,3)
-temp = d2.res_arr(:,:,11)./(d2.res_arr(:,:,13) + d2.res_arr(:,:,11));
-contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
+subplot (3,2,2)
+temp = d2.res_arr(:,:,11)./(d2.res_arr(:,:,10) + d2.res_arr(:,:,11));
+contour(d1.rp_arr,d1.p_obs_arr,temp',[0.22, 0.24, 0.26, 0.28, 0.3],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred proportion of','positive cases that are primary'})
-ylabel({'Observation probability (P_{obs})'},'FontSize',16)
+title({'Inferred proportion of','primary cases that are >19 years old'})
 %xlabel({'              Primary cases per cluster (R_p)'})
 
-% Inferred proportion primary among negative
+% Inferred proportion positive among secondary
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
 subplot (3,2,4)
-temp = d2.res_arr(:,:,10)./(d2.res_arr(:,:,12) + d2.res_arr(:,:,10));
+temp = d2.res_arr(:,:,13)./(d2.res_arr(:,:,12) + d2.res_arr(:,:,13));
 contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred proportion of','negative cases that are primary'})
+title({'Inferred proportion of','secondary cases that are >19 years old'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
 
 % Inferred r_effective
 subplot (3,2,5)
-contour(d1.rp_arr,d1.p_obs_arr,R_inf,'ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,R_inf,[0.55, 0.6, 0.65, 0.7, 0.75, 0.8],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
 title({'Effective reproduction number'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel('Primaries per cluster (R_p)')
+text(1.0,-0.08,0,'Primary cases per cluster (R_p)','FontSize', 11)
 
 % Inferred odds ratio
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
@@ -161,33 +102,19 @@ temp = d2.res_arr(:,:,11).*d2.res_arr(:,:,12)./(d2.res_arr(:,:,10) .* d2.res_arr
 % abs(temp) used because of some convergence difficulties yielding large negative
 % values of OR for high p_obs and r_p
 % contour(d1.rp_arr,d1.p_obs_arr,abs(temp)',[0.01 0.02 0.03 0.04 0.05 0.06],'ShowText','On','LineWidth',2)
-contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,temp',[4, 4.5, 5, 5.5, 6],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred odds ratio for','positive cases being primary'})
+title({'Inferred odds ratio for','>19 year old cases being primary'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel('Primaries per cluster (R_p)')
-text(.85,0.07,0,'Primary cases per cluster (R_p)','FontSize', 11)
+text(1.0,-0.08,0,'Primary cases per cluster (R_p)','FontSize', 11)
 
 % savepdf('Figures/040519_mpx_app')
+set(gcf, 'position', [10, 10, 550, 600])
 exportgraphics(gcf, 'Figures/082425_msls_app.pdf')
 
-%%
-% See script_disp050818 for the following figures:
-%
-% Figure for showing what the r_e is for various combinations of community
-% and hospital transmission
-%
-% Goal: For a few R_eff, plot reduction in incidence as a function of
-% decreased introduction and decreased transmission
 
-%%
-% See script_disp122018 for the following figures:
-%
-% Comparing MPX R in 1980s ot 2000s
-%
-% Analyzing MRSA data from Coll et al.
-
-% Figure look at R and OR for mpox in 2020s
+%% Figure look at R and OR for mpox in 2020s
 figure(1),clf
 
 clear
@@ -197,39 +124,40 @@ R_inf = squeeze(d1.rs_inf_res)';
 
 % Prob observed primary is a primary
 subplot (3,2,1)
-contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,5)','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,5)',[0.7, 0.8, 0.9, 1],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Probability that a case_ ','classified as a primary case_ ', 'is a primary case (P_{p\rightarrow p})'})
+title({'Probability that a case classified_ ','as primary is primary (P_{p\rightarrow p})'})
 %title({'P_{p\rightarrow p}'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
 
 % Prob observed secondary is a secondary
-subplot (3,2,2)
+subplot (3,2,3)
 contour(d1.rp_arr,d1.p_obs_arr,d2.res_arr(:,:,6)','ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Probability that a case_ ','classified as a secondary case_ ', 'is a secondary case (P_{s\rightarrow s})'})
+title({'Probability that a case classified_ ','as secondary is secondary (P_{s\rightarrow s})'})
 %title({'P_{s\rightarrow s}'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
+ylabel({'Observation probability (P_{obs})'},'FontSize',11)
 
 % Inferred proportion positive among primary
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
-subplot (3,2,3)
+subplot (3,2,2)
 temp = d2.res_arr(:,:,11)./(d2.res_arr(:,:,10) + d2.res_arr(:,:,11));
-contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,temp',[0.55, 0.56, 0.57, 0.58],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred proportion of','primary cases that are positive'})
-ylabel({'Observation probability (P_{obs})'},'FontSize',16)
+title({'Inferred proportion of primary','cases that report animal contact'})
+
 %xlabel({'              Primary cases per cluster (R_p)'})
 
 % Inferred proportion positive among secondary
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
 subplot (3,2,4)
 temp = d2.res_arr(:,:,13)./(d2.res_arr(:,:,12) + d2.res_arr(:,:,13));
-contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,temp',[0.42, 0.43, 0.44, 0.45],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred proportion of','secondary cases that are positive'})
+title({'Inferred proportion of secondary','cases that report animal contact'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel({'              Primary cases per cluster (R_p)'})
 
@@ -240,6 +168,7 @@ set(gca,'FontSize',9)
 title({'Effective reproduction number'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel('Primaries per cluster (R_p)')
+text(1.0,-0.08,0,'Primary cases per cluster (R_p)','FontSize', 11)
 
 % Inferred odds ratio
 % res_arr(:,:,10:13) = [Q_pn Q_pp Q_sn Q_sp];
@@ -249,28 +178,13 @@ temp = d2.res_arr(:,:,11).*d2.res_arr(:,:,12)./(d2.res_arr(:,:,10) .* d2.res_arr
 % abs(temp) used because of some convergence difficulties yielding large negative
 % values of OR for high p_obs and r_p
 % contour(d1.rp_arr,d1.p_obs_arr,abs(temp)',[0.01 0.02 0.03 0.04 0.05 0.06],'ShowText','On','LineWidth',2)
-contour(d1.rp_arr,d1.p_obs_arr,temp','ShowText','On','LineWidth',2)
+contour(d1.rp_arr,d1.p_obs_arr,temp',[1.5, 1.55, 1.6, 1.65, 1.7],'ShowText','On','LineWidth',2)
 set(gca,'FontSize',9)
-title({'Inferred odds ratio for','primary cases being positive'})
+title({'Inferred odds ratio for primary','cases reporting animal contact'})
 %ylabel({'Observation_ ','probability (P_{obs})'})
 %xlabel('Primaries per cluster (R_p)')
-text(.85,0.07,0,'Primary cases per cluster (R_p)','FontSize', 11)
+text(1.0,-0.08,0,'Primary cases per cluster (R_p)','FontSize', 11)
 
 % savepdf('Figures/040519_mpx_app')
+set(gcf, 'position', [10, 10, 550, 600])
 exportgraphics(gcf, 'Figures/082425_mpox_app.pdf')
-
-%%
-% See script_disp050818 for the following figures:
-%
-% Figure for showing what the r_e is for various combinations of community
-% and hospital transmission
-%
-% Goal: For a few R_eff, plot reduction in incidence as a function of
-% decreased introduction and decreased transmission
-
-%%
-% See script_disp122018 for the following figures:
-%
-% Comparing MPX R in 1980s ot 2000s
-%
-% Analyzing MRSA data from Coll et al.
